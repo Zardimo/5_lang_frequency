44df098a9cb0d955bb49891aab04fe4a8b9781d3
Zardimo <vasyasapeggin@rambler.ru>
2019-02-12 11:14:57 +0500

    Задача Решена

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+This program created for output 10 most popular words in text file.
+
+# Quickstart
+
+Specify the path for text file
+
+```bash
+c:\Users\User\5_lang_frequency\lang_frequency.py c:\Users\User\proba_1.txt
+```
+
+In output you see next:
+
+```bash
+[('в', 6), ('и', 5), ('направлений', 4), ('деятельности', 4), ('формировании', 3), ('дальнейших', 3), ('системы', 3), ('что', 3), ('позволяет', 3), ('обеспечивает', 2)]
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,29 @@
+from collections import Counter
+import os
+from sys import argv
+import sys
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, encoding="utf8") as file:
+        lines = file.read()
+    return lines
 
+def get_most_frequent_words(python_object):
+    try:
+        count_words = Counter(python_object.split())
+    except AttributeError:
+        return None
+    return count_words.most_common(10)
 
-def get_most_frequent_words(text):
-    pass
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) != 2:
+        exit('Укажите путь к файлу')
+    python_object = load_data(argv[1])
+    if not python_object:
+        exit("Укажите верный путь к файлу")
+    print(get_most_frequent_words(python_object))
